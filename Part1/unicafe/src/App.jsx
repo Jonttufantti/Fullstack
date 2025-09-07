f9d257ec959809d2717f3cc5aabc736564a77fcf
Joona Sandbacka <joona.sandbacka@hotmail.fi>
2025-09-07 16:30:03 +0300

    Unicafe 1.7

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -11,37 +11,31 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good+bad+neutral
+  const average = total ? (good*1+bad*-1+neutral*0)/total : 0
+  const goodPercent = total ? (good/total*100).toFixed(2) : 0
 
-  const handleGoodclick = () => {
-    const newGood = good + 1;
-    setGood(newGood);
-    console.log(newGood);
-  };
-
-  const handleNeutralclick = () => {
-    const newNeutral = neutral + 1;
-    setNeutral(newNeutral);
-    console.log(newNeutral);
-  };
-
-  const handleBadclick = () => {
-    const newBad = bad + 1;
-    setBad(newBad);
-    console.log(newBad);
-  };
+  const handleClick = (feedback) => {
+    if (feedback==="good") setGood(good+1)
+    if (feedback==="neutral") setNeutral(neutral+1)
+    if (feedback==="bad") setBad(bad+1)
+  }
 
   return (
     <div>
       <h1>Give feedback</h1>
       <br />
-      <Button onClick={handleGoodclick} text="Good" />
-      <Button onClick={handleNeutralclick} text="Neutral" />
-      <Button onClick={handleBadclick} text="Bad" />
+      <Button onClick={()=>handleClick("good")} text="Good" />
+      <Button onClick={()=>handleClick("neutral")} text="Neutral" />
+      <Button onClick={()=>handleClick("bad")} text="Bad" />
       <br />
       <h1>Statistics</h1>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {total}</p>
+      <p>Average {average}</p>
+      <p>Positive {goodPercent}%</p>
     </div>
   )
 }
