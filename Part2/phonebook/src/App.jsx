3bc7b1e430a5651e4474398fc4f4e3d4cc696c49
Joona Sandbacka <joona.sandbacka@hotmail.fi>
2025-09-10 17:15:05 +0300

    2.11: The Phonebook

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
-const App = (props) => {
-  const [persons, setPersons] = useState(props.persons)
+const App = () => {
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchPersons, setSearchPersons] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -50,8 +61,8 @@ const App = (props) => {
   return (
     <div>
       <h2>Phonebook</h2>
-      
-      <Filter search={searchPersons} handleSearchPersons={handleSearchPersons}/>
+
+      <Filter search={searchPersons} handleSearchPersons={handleSearchPersons} />
 
 
       <h2>Add new</h2>
