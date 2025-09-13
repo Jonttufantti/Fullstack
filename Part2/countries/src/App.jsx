b5a928168b3a9a9ecb716d4627cf74ad7659a455
Joona Sandbacka <joona.sandbacka@hotmail.fi>
2025-09-13 20:21:32 +0300

    2.20*: Data for countries

--- Part2/countries/src/App.jsx
+++ Part2/countries/src/App.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react'
 import getAll from './services/countries'
-import { hasFlag } from 'country-flag-icons'
 
 function App() {
   const [countries, setCountries] = useState([])
   const [searchCountries, setSearchCountries] = useState('')
   const [header, setHeader] = useState('Search results')
   const [countryToShow, setCountryToShow] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   const countriesToShow = countries.filter(country =>
     country.name.common.toLowerCase().includes(searchCountries.toLowerCase())
   )
+  const apiKey = import.meta.env.VITE_WEATHERMAP_KEY;
 
   useEffect(() => {
     setCountryToShow(null);
@@ -21,10 +22,25 @@ function App() {
       setHeader(countriesToShow[0].name.common);
     } else if (countryToShow) {
       setHeader(countryToShow.name.common);
+
     } else {
       setHeader('Search results');
     }
   }, [countriesToShow, setHeader]);
+
+  useEffect(() => {
+    console.log('UseEffect weather');
+    const country = countryToShow || (countriesToShow.length === 1 ? countriesToShow[0] : null);
+    if (!country) return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${apiKey}&units=metric`)
+      .then(res => res.json())
+      .then(data => setWeather(data))
+      .catch(err => console.error(err));
+
+  }, [countryToShow, countriesToShow.length, countriesToShow[0]?.name.common]);
+
+
 
   const Notification = () => {
     if (countriesToShow.length > 10 && searchCountries) {
@@ -74,6 +90,19 @@ function App() {
         src={country.flags.svg}
         width="150"
       />
+      {weather && (
+        <div>
+          <h3>Weather in {header}</h3>
+          <p>Temperature: {weather.main.temp} °C</p>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
+          <p>Wind: {weather.wind.speed} m/s</p>
+          <p>Condition: {weather.weather[0].description}</p>
+        </div>
+      )}
+
     </div>
   );
 
