0666cd20d4a89069c85e13a9087b7ba66706580b
Joona Sandbacka <joona.sandbacka@hotmail.fi>
2025-09-14 18:15:31 +0300

    3.5: Phonebook backend

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -4,27 +4,32 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
-      id: "1",
-      name: "Arto Hellas", 
-      number: "040-123456"
+    {
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456"
     },
-    { 
-      id: "2",
-      name: "Ada Lovelace", 
-      number: "39-44-5323523"
+    {
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
     },
-    { 
-      id: "3",
-      name: "Dan Abramov", 
-      number: "12-43-234345"
+    {
+        id: "3",
+        name: "Dan Abramov",
+        number: "12-43-234345"
     },
-    { 
-      id: "4",
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
+    {
+        id: "4",
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
     }
 ]
+
+const generateId = () => {
+    return String(Math.floor(Math.random() * 1000000))
+}
+
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
@@ -41,7 +46,6 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-
 app.get('/info', (request, response) => {
     const numberOfPeople = persons.length
     const timeOfReq = new Date()
@@ -51,9 +55,29 @@ app.get('/info', (request, response) => {
     )
 })
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person);
+})
+
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(note => note.id !== id)
+    persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
 })
