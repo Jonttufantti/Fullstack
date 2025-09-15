e8d09bdddace5ff0a9b350be55a6ffb711ec28a0
Joona Sandbacka <joona.sandbacka@hotmail.fi>
2025-09-15 17:27:12 +0300

    3.9.-3.11 Phonebook backend

---
+++ Part3/phonebook_backend/README.md
@@ -0,0 +1,3 @@
+https://phonebook-backend-ancient-breeze-1387.fly.dev/
+
+https://phonebook-back-end-ppjn.onrender.com/
--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -6,8 +6,8 @@ const path = require('path')
 const app = express()
 
 app.use(express.json())
-app.use(express.static('dist'))
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => { 
     return req.method === 'POST' ? JSON.stringify(req.body) : ''; 
