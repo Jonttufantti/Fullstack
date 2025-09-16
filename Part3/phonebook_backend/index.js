544bdc32803dfec13e8919d7c54b3305b2fc01aa
Joona Sandbacka <joona.sandbacka@hotmail.fi>
2025-09-16 20:39:33 +0300

    3.18*: Phonebook database

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -28,35 +28,6 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
-
-/*
-const generateId = () => {
-    return String(Math.floor(Math.random() * 1000000))
-}
-*/
-
 
 app.get('/', (req, res) => {
     if (!req.path.startsWith('/api')) {
@@ -84,14 +55,18 @@ app.get('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-    const numberOfPeople = persons.length
-    const timeOfReq = new Date()
-    response.send(
-        `<p>Phonebook has info for ${numberOfPeople} people</p>
+app.get('/info', (request, response, next) => {
+  Person.countDocuments({})
+    .then(count => {
+      const timeOfReq = new Date()
+      response.send(
+        `<p>Phonebook has info for ${count} people</p>
         <p>${timeOfReq}</p>`
-    )
+      )
+    })
+    .catch(error => next(error))
 })
+
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
