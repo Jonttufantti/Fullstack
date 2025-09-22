6b4b21196156bdba6cdc39f01c3b987035e039dd
Joona Sandbacka <joona.sandbacka@hotmail.fi>
2025-09-22 13:41:17 +0300

    4.5*: Helper Functions and Unit Tests

--- Part4/blog_list/utils/list_helper.js
+++ Part4/blog_list/utils/list_helper.js
@@ -8,8 +8,14 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
-module.exports = {
-  dummy, totalLikes
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return 0;
+
+  return blogs.reduce((mostLiked, currentObject) => {
+    return currentObject.likes > mostLiked.likes ? currentObject : mostLiked;
+  }, blogs[0]);
 }
 
-
+module.exports = {
+  dummy, totalLikes, favoriteBlog
+}
