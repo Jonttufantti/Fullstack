6290a649f95e6ffc09f48f240494032a776636a2
Joona Sandbacka <joona.sandbacka@hotmail.fi>
2025-10-02 12:30:41 +0300

    5.7 Blog List Frontend

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [details, setDetails] = useState(false)
+
+  const viewDetails = { display: details ? '' : 'none' }
+
+  const toggleDetails = () => {
+    setDetails(!details)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetails}>{details ? 'Hide' : 'View'}</button>
+      </div>
+      <div style={viewDetails}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user?.name}</div>
+      </div>
+    </div>
+  )
+}
+
 
 export default Blog
