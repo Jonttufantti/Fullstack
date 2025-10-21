56797d15a3dfde45bf5e71fb5a5713bac063357e
Joona <joona.sandbacka@hotmail.fi>
2025-10-21 13:37:28 +0300

    8.1: The number of books and authors

--- Part8/apollo/index.js
+++ Part8/apollo/index.js
@@ -40,6 +40,17 @@ type Person {
   id: ID!
 }
 
+enum YesNo {
+  YES
+  NO  
+}
+
+type Query {
+  personCount: Int!
+  allPersons(phone: YesNo): [Person!]!    
+  findPerson(name: String!): Person
+}
+
 type Query {
   personCount: Int!
   allPersons: [Person!]!
@@ -53,14 +64,27 @@ type Mutation {
     street: String!
     city: String!
   ): Person
+  editNumber(
+    name: String!
+    phone: String!
+  ): Person
 }
 `;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
-    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons
+      }
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
+    findPerson: (root, args) =>
+      persons.find(p => p.name === args.name)
   },
   Person: {
     address: (root) => {
@@ -85,6 +109,16 @@ const resolvers = {
       persons = persons.concat(person);
       return person;
     },
+    editNumber: (root, args) => {
+      const person = persons.find(p => p.name === args.name)
+      if (!person) {
+        return null
+      }
+
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+      return updatedPerson
+    }
   },
 };
 
