3a6980d2172fa7781c89a9b19e04bf1f80471224
Joona <joona.sandbacka@hotmail.fi>
2025-10-25 18:36:36 +0300

    8.19 Books by genre, part 1

--- Part8/library-frontend/src/components/Books.jsx
+++ Part8/library-frontend/src/components/Books.jsx
@@ -1,20 +1,41 @@
+import { useState } from "react";
+
 const Books = ({ show, books }) => {
+  const [selectedGenre, setSelectedGenre] = useState(null);
+
   if (!show) {
     return null;
   }
+
+  const genres = Array.from(new Set(books.flatMap((b) => b.genres || [])));
+
+  console.log("Genres", { genres });
+
+  const filteredBooks = selectedGenre
+    ? books.filter((b) => b.genres.includes(selectedGenre))
+    : books;
 
   return (
     <div>
       <h2>books</h2>
 
+      <div>
+        {genres.map((g) => (
+          <button key={g} onClick={() => setSelectedGenre(g)}>
+            {g}
+          </button>
+        ))}
+        <button onClick={() => setSelectedGenre(null)}>all genres</button>
+      </div>
+
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>title</th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
