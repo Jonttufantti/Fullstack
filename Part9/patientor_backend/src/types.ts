18054a162c58d02b3f59016fed9ca723e67594ed
Joona <joona.sandbacka@hotmail.fi>
2025-11-04 18:30:01 +0200

    9.23: Patientor

--- Part9/patientor_backend/src/types.ts
+++ Part9/patientor_backend/src/types.ts
@@ -1,6 +1,15 @@
 import { z } from "zod";
 
-export interface Entry {}
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other",
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Diagnosis {
   code: string;
@@ -14,10 +23,46 @@ export const NewPatientSchema = z.object({
   name: z.string(),
   dateOfBirth: z.string(),
   ssn: z.string(),
-  gender: z.enum(["male", "female", "other"]),
+  gender: z.enum(Gender),
   occupation: z.string(),
   entries: z.array(z.any()),
 });
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
 
 export type NewPatientEntry = z.infer<typeof NewPatientSchema>;
 
